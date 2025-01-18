76474a3a4eac138a69c81fd941530c2f1f058134
Mustafa Zaidani <mustafamari20@gmail.com>
2025-01-19 00:26:59 +0300

    refactor: removed cache flag from commands in lint-staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,9 +1,7 @@
 const config = {
   '*.{ts,tsx}': ["bash -c 'pnpm check-types'"],
-  '*.{js,cjs,mjs,jsx,ts,tsx,json}': ['eslint --cache --fix'],
-  '*.{js,cjs,mjs,jsx,ts,tsx,css}': [
-    'prettier --cache --write --ignore-unknown',
-  ],
+  '*.{js,cjs,mjs,jsx,ts,tsx,json}': ['eslint --fix'],
+  '*.{js,cjs,mjs,jsx,ts,tsx,css}': ['prettier --write --ignore-unknown'],
 };
 
 export default config;
